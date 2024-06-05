31730f287d15d3531e9f861e64dbed5f3209a8b3
Everlon Passos <dev@everlon.com.br>
2024-06-05 19:13:51 -0300

    Middleware com validação Yup e sua tradução

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import { router } from './routes';
 import 'dotenv/config'
+import './shared/services/TranslationYup';
+import { router } from './routes';
+
 
 const server = express();
 
--- src/server/controllers/marcas/Create.ts
+++ src/server/controllers/marcas/Create.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
 
 
@@ -10,21 +11,29 @@ const bodyValidation: yup.Schema<IMarcas> = yup.object().shape({
   nome: yup.string().required().min(3),
 })
 
-export const create = async (req: Request<{}, {}, IMarcas>, res: Response) => {
 
-  let validatedData: IMarcas | undefined = undefined;
-
+// Middleware
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    await bodyValidation.validate(req.body, { abortEarly: false});
+    return next(); // Envia para a próxima
+
   } catch (error) {
     const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
+    yupError.inner.forEach(error => {
+      if (!error.path) return;
+      validationErrors[error.path] = error.message;
     })
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors: validationErrors })
   }
+}
+
+
+// Controller
+export const create = async (req: Request<{}, {}, IMarcas>, res: Response) => {
 
   console.log(req.body.nome);
 
