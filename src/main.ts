a4dc6abd146337deef575941320662e4d3517f0a
Everlon Passos <dev@everlon.com.br>
2024-06-07 10:01:29 -0300

    Implantando Seed

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
+import { Knex } from '../src/server/database/knex';
 import { server } from './server/Server';
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`Running... PORT: ${process.env.PORT || 3333} `);
-});
+
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+  });
+};
+
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  console.log('Rodando Migrations e Seeds');
+
+  Knex.migrate.latest().then(() => {
+      Knex.seed.run().then(() => startServer()).catch(console.log);
+    }).catch(console.log);
+} else {
+  startServer();
+}
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -5,6 +5,7 @@ import { Knex } from '../src/server/database/knex';
 
 beforeAll(async () => {
   await Knex.migrate.latest();
+  await Knex.seed.run();
 });
 
 afterAll(async () => {
